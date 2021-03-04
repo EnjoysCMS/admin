d6e2a7862f7e6b0df83a0135212dee3d00720493
enjoys <deadly.info@gmail.com>
2021-03-04 21:45:53 +0300

    setting...

--- src/Controller/Setting.php
+++ src/Controller/Setting.php
@@ -5,13 +5,41 @@ namespace App\Module\Admin\Controller;
 
 
 use App\Module\Admin\BaseController;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\ObjectRepository;
+use Enjoys\Forms\Renderer\RendererInterface;
+use Enjoys\Http\ServerRequestInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Twig\Environment;
 
 class Setting extends BaseController
 {
+    /**
+     * @var ObjectRepository
+     */
+    private ObjectRepository $settingRepository;
+
+    public function __construct(
+        Environment $twig,
+        ServerRequestInterface $serverRequest,
+        EntityManager $entityManager,
+        UrlGeneratorInterface $urlGenerator,
+        RendererInterface $renderer
+    ) {
+        parent::__construct($twig, $serverRequest, $entityManager, $urlGenerator, $renderer);
+        $this->settingRepository = $this->entityManager->getRepository(\App\Entities\Setting::class);
+    }
+
     public function setting()
     {
         return $this->view('@a/setting/setting.twig', $this->getContext(
-            new \App\Module\Admin\Core\Setting()
+            new \App\Module\Admin\Core\Setting(
+                $this->settingRepository,
+                $this->entityManager,
+                $this->serverRequest,
+                $this->urlGenerator,
+                $this->renderer
+            )
         ));
     }
 }
--- src/Core/Setting.php
+++ src/Core/Setting.php
@@ -4,14 +4,101 @@
 namespace App\Module\Admin\Core;
 
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\ObjectRepository;
+use Enjoys\Forms\Form;
+use Enjoys\Forms\Renderer\RendererInterface;
+use Enjoys\Http\ServerRequestInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+use function DI\string;
+
 class Setting implements ModelInterface
 {
 
+
+    /**
+     * @var ObjectRepository
+     */
+    private ObjectRepository $settingRepository;
+    /**
+     * @var EntityManager
+     */
+    private EntityManager $entityManager;
+    /**
+     * @var ServerRequestInterface
+     */
+    private ServerRequestInterface $serverRequest;
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private UrlGeneratorInterface $urlGenerator;
+    /**
+     * @var RendererInterface
+     */
+    private RendererInterface $renderer;
+
+    public function __construct(
+        ObjectRepository $settingRepository,
+        EntityManager $entityManager,
+        ServerRequestInterface $serverRequest,
+        UrlGeneratorInterface $urlGenerator,
+        RendererInterface $renderer
+    ) {
+        $this->settingRepository = $settingRepository;
+        $this->entityManager = $entityManager;
+        $this->serverRequest = $serverRequest;
+        $this->urlGenerator = $urlGenerator;
+        $this->renderer = $renderer;
+    }
+
     public function getContext(): array
     {
+        $form = $this->getForm();
+        $this->renderer->setForm($form);
         return [
-            'sitename' => \App\Components\Helpers\Setting::get('sitename'),
-            'sitename2' => \App\Components\Helpers\Setting::get('yandex-metrica'),
+            'form' => $this->renderer
         ];
+    }
+
+    private function getForm()
+    {
+        $form = new Form();
+        $form->setDefaults($this->settingRepository->findAllKeyVar());
+        $settings = (array)$this->settingRepository->findAll();
+
+
+        /** @var \App\Entities\Setting $setting */
+        foreach ($settings as $setting) {
+            switch ($setting->getType()) {
+                case 'radio':
+                    $form->radio($setting->getVar(), $setting->getName())
+                        ->setDescription((string)$setting->getDescription())
+                        ->fill(json_decode($setting->getParams(), true));
+
+                    unset($data);
+                    break;
+                case 'select':
+                    $form->select($setting->getVar(), $setting->getName())
+                        ->setDescription((string)$setting->getDescription())
+                        ->fill(json_decode($setting->getParams(), true));
+                    break;
+                case 'textarea':
+                    $form->textarea($setting->getVar(), $setting->getName())
+                        ->setDescription((string)$setting->getDescription());
+                    break;
+                case 'text':
+                default:
+                    $form->text($setting->getVar(), $setting->getName())
+                        ->setDescription((string)$setting->getDescription());
+                    break;
+            }
+        }
+        return $form;
+        // die();
+    }
+
+    private function doAction()
+    {
     }
 }
