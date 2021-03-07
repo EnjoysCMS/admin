9020e0902f1b9af5480e8e8eca7d258d7fbabea5
enjoys <deadly.info@gmail.com>
2021-03-07 21:20:11 +0300

    remove customize setting

--- template/assets/custom.js
+++ template/assets/custom.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
     $('.nav-sidebar').addClass('nav-flat');
     $('.nav-sidebar').addClass('nav-child-indent');
 
+    let url = window.location;
+    let element = $('.nav-link').filter(function () {
+        return this.href === url || url.href.indexOf(this.href) === 0;
+    }).addClass('active').parents().addClass('menu-open');
+
 });
