7d7f69ec88c301e19ebe3a01f01d571177579a35
enjoys <deadly.info@gmail.com>
2023-06-10 15:13:20 +0300

    refactor Config.php

--- src/Config.php
+++ src/Config.php
@@ -6,34 +6,61 @@ declare(strict_types=1);
 namespace EnjoysCMS\Module\Admin;
 
 
-use DI\DependencyException;
-use DI\FactoryInterface;
-use DI\NotFoundException;
-use EnjoysCMS\Core\Components\Modules\ModuleConfig;
+use EnjoysCMS\Core\Components\Modules\ModuleCollection;
+use Exception;
+use InvalidArgumentException;
+use Symfony\Component\Yaml\Yaml;
 
 final class Config
 {
 
-    private ModuleConfig $config;
+    private const MODULE_NAME = 'enjoyscms/admin';
+
 
     /**
-     * @throws DependencyException
-     * @throws NotFoundException
+     * @throws Exception
      */
-    public function __construct(FactoryInterface $factory)
+    public function __construct(
+        private readonly \Enjoys\Config\Config $config,
+        ModuleCollection $moduleCollection
+    ) {
+        $module = $moduleCollection->find(self::MODULE_NAME) ?? throw new InvalidArgumentException(
+            sprintf(
+                'Module %s not found. Name must be same like packageName in module composer.json',
+                self::MODULE_NAME
+            )
+        );
+
+
+        if (file_exists($module->path . '/config.yml')) {
+            $config->addConfig(
+                [
+                    self::MODULE_NAME => file_get_contents($module->path . '/config.yml')
+                ],
+                ['flags' => Yaml::PARSE_CONSTANT],
+                \Enjoys\Config\Config::YAML,
+                false
+            );
+        }
+    }
+
+    public function get(string $key = null, mixed $default = null): mixed
     {
-        $this->config = $factory->make(ModuleConfig::class, ['moduleName' => 'enjoyscms/admin']);
+        if ($key === null) {
+            return $this->config->get(self::MODULE_NAME);
+        }
+        return $this->config->get(sprintf('%s->%s', self::MODULE_NAME, $key), $default);
     }
 
 
-    public function getModuleConfig(): ModuleConfig
+    public function all(): array
     {
-        return $this->config;
+        return $this->config->get();
     }
 
-    public function getContentEditorConfigParamForCustomBlocks()
+    public function getContentEditorConfigParamForCustomBlocks(): string|array|null
     {
-        return $this->config->get('editor')['custom_blocks'] ?? null;
+        return $this->get('editor->custom_blocks');
     }
 
 }
