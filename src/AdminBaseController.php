f733dd0a5bc26488b7499d9f27bdaa594799e160
enjoys <deadly.info@gmail.com>
2023-08-12 23:25:27 +0300

    update code with AbstractController

--- src/AdminBaseController.php
+++ src/AdminBaseController.php
@@ -9,23 +9,15 @@ use DI\DependencyException;
 use DI\NotFoundException;
 use Enjoys\Forms\Interfaces\RendererInterface;
 use Enjoys\Forms\Renderer\Bootstrap4\Bootstrap4Renderer;
-use EnjoysCMS\Core\Breadcrumbs\BreadcrumbCollection;
+use EnjoysCMS\Core\AbstractController;
 use EnjoysCMS\Core\Modules\ModuleCollection;
-use EnjoysCMS\Core\Setting\Setting;
 use Exception;
-use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\Routing\RouteCollection;
-use Twig\Environment;
 
-abstract class AdminBaseController
+abstract class AdminBaseController extends AbstractController
 {
 
     public const UUID_RULE_REQUIREMENT = '[0-9a-f]{8}-[0-9a-f]{4}-[13-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}';
-
-    protected Environment $twig;
-    protected Setting $setting;
-    protected ResponseInterface $response;
-    protected BreadcrumbCollection $breadcrumbs;
 
     /**
      * @throws DependencyException
@@ -35,11 +27,7 @@ abstract class AdminBaseController
     public function __construct(
         protected Container $container,
     ) {
-        $this->twig = $this->container->get(Environment::class);
-        $this->setting = $this->container->get(Setting::class);
-        $this->response = $this->container->get(ResponseInterface::class);
-        $this->breadcrumbs = $this->container->get(BreadcrumbCollection::class);
-
+        parent::__construct($container);
         $this->container->set(RendererInterface::class, new Bootstrap4Renderer());
 
         $this->twig->getLoader()->addPath(__DIR__ . '/../template', 'a');
@@ -59,19 +47,5 @@ abstract class AdminBaseController
             'routeCollection',
             $this->container->get(RouteCollection::class)
         );
-    }
-
-
-    protected function response(string $body): ResponseInterface
-    {
-        $this->response->getBody()->write($body);
-        return $this->response;
-    }
-
-    protected function jsonResponse(mixed $payload): ResponseInterface
-    {
-        $this->response = $this->response->withHeader('Content-Type', 'application/json');
-        $this->response->getBody()->write(json_encode($payload));
-        return $this->response;
     }
 }
