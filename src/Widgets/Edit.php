f13d844af4534f13e7b4931f639d2d218cd805ab
enjoys <deadly.info@gmail.com>
2023-08-16 12:19:11 +0300

    fix: edit widgets

--- src/Widgets/Edit.php
+++ src/Widgets/Edit.php
@@ -6,13 +6,16 @@ declare(strict_types=1);
 namespace EnjoysCMS\Module\Admin\Widgets;
 
 
+use DI\Container;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Exception\NotSupported;
 use Doctrine\ORM\Exception\ORMException;
 use Doctrine\ORM\NoResultException;
 use Doctrine\ORM\OptimisticLockException;
+use Enjoys\Forms\AttributeFactory;
 use Enjoys\Forms\Form;
 use EnjoysCMS\Core\Block\Entity\Widget;
+use Invoker\Exception\NotCallableException;
 use Psr\Http\Message\ServerRequestInterface;
 
 final class Edit
@@ -27,6 +30,7 @@ final class Edit
     public function __construct(
         private readonly EntityManager $em,
         private readonly ServerRequestInterface $request,
+        private readonly Container $container,
     ) {
         $this->widget = $this->em->getRepository(Widget::class)->find(
             $this->request->getAttribute('id')
@@ -36,40 +40,77 @@ final class Edit
 
     public function getForm(): Form
     {
-        $options = $this->widget->getOptions();
-        unset($options['gs']);
-
         $form = new Form();
-        $form->setDefaults(
-            function () use ($options) {
-                $data = [];
-                foreach ($options as $key => $value) {
-                    $data[$key] = $value['value'] ?? (!is_array($value) ? $value : null) ?? null;
-                }
-                return $data;
-            }
-        );
+        $form->setDefaults([
+            'options' => $this->widget->getOptionsKeyValue(),
+        ]);
 
-        foreach ($options as $key => $value) {
-            switch ($value['type'] ?? 'text') {
-                case 'radio':
-                    $form->radio($key, $value['title'] ?? ucfirst($key))
-                        ->setDescription($value['description'] ?? '')
-                        ->fill($value['data'] ?? [], true);
-                    break;
-                case 'select':
-                    $form->select($key, $value['title'] ?? ucfirst($key))
-                        ->setDescription($value['description'] ?? '')
-                        ->fill($value['data'] ?? [], true);
-                    break;
-                case 'text':
-                default:
-                    $form->text($key, $value['title'] ?? ucfirst($key))
-                        ->setDescription($value['description'] ?? '');
-                    break;
+        foreach ($this->widget->getOptions() as $key => $option) {
+            if ($key === 'gs') {
+                continue;
             }
+            $type = $option['type'] ?? null;
+
+            if ($type) {
+                $data = $option['data'] ?? [null];
+                try {
+                    if (is_array($data) && !array_key_exists(0, $data)) {
+                        throw new NotCallableException();
+                    }
+                    $data = $this->container->call($data);
+                } catch (NotCallableException) {
+                    //skip
+                }
+                switch ($type) {
+                    case 'radio':
+                        $form->radio(
+                            "options[$key]",
+                            (isset($option['name'])) ? $option['name'] : $key
+                        )->setDescription(
+                            $option['description'] ?? ''
+                        )->fill($data, true);
+                        break;
+                    case 'checkbox':
+                        $form->checkbox(
+                            "options[$key]",
+                            (isset($option['name'])) ? $option['name'] : $key
+                        )->setDescription(
+                            $option['description'] ?? ''
+                        )->fill($data, true);
+                        break;
+                    case 'select':
+                        $form->select(
+                            "options[$key]",
+                            (isset($option['name'])) ? $option['name'] : $key
+                        )->setDescription(
+                            $option['description'] ?? ''
+                        )->fill($data, true);
+                        break;
+                    case 'textarea':
+                        $form->textarea(
+                            "options[$key]",
+                            (isset($option['name'])) ? $option['name'] : $key
+                        )->setDescription($option['description'] ?? '');
+                        break;
+                    case 'file':
+                        $form->file("options[$key]", $option['name'] ?? $key)
+                            ->setDescription($option['description'] ?? '')
+                            ->setMaxFileSize(
+                                $data['max_file_size'] ?? iniSize2bytes(ini_get('upload_max_filesize'))
+                            )
+                            ->setAttributes(AttributeFactory::createFromArray($data['attributes'] ?? []));
+                        break;
+                }
+
+                continue;
+            }
+            $form->text("options[$key]", (isset($option['name'])) ? $option['name'] : $key)->setDescription(
+                $option['description'] ?? ''
+            );
         }
-        $form->submit('save', 'Сохранить');
+
+        $form->submit('save');
+
         return $form;
     }
 
@@ -79,15 +120,11 @@ final class Edit
      */
     public function doAction(): void
     {
-        $result = [];
-        foreach ($this->request->getParsedBody() as $key => $value) {
-            if (!in_array($key, array_keys($this->widget->getOptions()))) {
-                continue;
-            }
-            $result[$key]['value'] = empty($value) ? null : $value;
-        }
+        $options = $this->widget->getOptions();
+        $options->setValues($this->request->getParsedBody()['options'] ?? []);
 
-        $this->widget->setOptions(array_merge_recursive_distinct($this->widget->getOptions(), $result));
+        $this->widget->setOptions($options);
+
         $this->em->flush();
     }
 
